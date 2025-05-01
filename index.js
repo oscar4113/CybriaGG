66104cc9d7e2202a7c655db2f477c74d17080e1b
oscar4113 <65124475+oscar4113@users.noreply.github.com>
2025-05-01 20:36:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://thetrignometry.com';
+const nggUrl = 'https://now.gg';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'thetrignometry.com') {
+    if (req.headers.host === 'now.gg') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
